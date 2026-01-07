933c30d6853106488880a9f96f916fd561460d90
Nazar Zhyliuk <63409065+softK1T@users.noreply.github.com>
2026-01-07 17:12:57 +0000

    Update tailwind config with fonts and colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,21 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Arimo', 'sans-serif'],
+        heading: ['Poppins', 'sans-serif'],
+        ui: ['Montserrat', 'sans-serif'],
+      },
+      colors: {
+        axilon: {
+          yellow: '#FFB600',
+          black: '#1a1a1a',
+          dark: '#111111',
+          gray: '#f5f5f5'
+        }
+      }
+    },
   },
   plugins: [],
 }
